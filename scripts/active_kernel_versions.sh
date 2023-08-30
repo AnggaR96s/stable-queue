b8d69313a0390e8e4e8de6dff817a0e6e31116e0
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2023-08-30 17:32:22 +0200

    scripts: have a way to pick the active kernel versions
    And track them in the stable queue repo itself.

---
+++ scripts/active_kernel_versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REAL_SCRIPT=$(realpath -e "${BASH_SOURCE[0]}")
+SCRIPT_TOP="${SCRIPT_TOP:-$(dirname "${REAL_SCRIPT}")}"
+
+for VERSION in $(cat ${SCRIPT_TOP}/../active_kernel_versions); do
+	echo -n "${VERSION} "
+done
+echo ""
+
---
+++ scripts/active_kernel_versions_get.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REAL_SCRIPT=$(realpath -e "${BASH_SOURCE[0]}")
+SCRIPT_TOP="${SCRIPT_TOP:-$(dirname "${REAL_SCRIPT}")}"
+
+VERSIONS=$(${SCRIPT_TOP}/active_kernel_versions.sh)
+V=$(gum choose --no-limit ${VERSIONS})
+echo "${V}"
+exit
